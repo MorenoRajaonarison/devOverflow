1655b37639ebdd29eef00faef6f6373f1d1ff6e5
Rajaonarison <rajaonarison.moreno@gmail.com>
2024-08-09 15:06:12 +0200

    wip: Manage Filter state

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -2,9 +2,35 @@
 
 import { HomePageFilters } from "@/constants/filter";
 import { Button } from "../ui/button";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { formUrlQuery } from "@/lib/utils";
 
 const HomeFilters = () => {
-  const active = "newest";
+  const searchParams = useSearchParams();
+  const [active, setActive] = useState("");
+  const router = useRouter();
+
+  const handleFilterChange = (item: string) => {
+    if (active === item) {
+      setActive("");
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: null,
+      });
+      router.push(newUrl, { scroll: false });
+    } else {
+      setActive(item);
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: item.toLowerCase(),
+      });
+      router.push(newUrl, { scroll: false });
+    }
+  };
+
   return (
     <div className="mt-10 hidden flex-wrap gap-3 md:flex">
       {HomePageFilters.map((filter) => (
@@ -15,7 +41,7 @@ const HomeFilters = () => {
               : "bg-light-800 text-light-500"
           }`}
           key={filter.value}
-          onClick={() => {}}
+          onClick={() => handleFilterChange(filter.value)}
         >
           {filter.name}
         </Button>
