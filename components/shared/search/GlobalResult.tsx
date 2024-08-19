782cf15360a770941ba8fa38282a36b85ec2fe8a
Rajaonarison <rajaonarison.moreno@gmail.com>
2024-08-19 11:35:23 +0200

    wip: Create GlobalSearch Result Component

--- components/shared/search/GlobalResult.tsx
+++ components/shared/search/GlobalResult.tsx
@@ -1,11 +1,94 @@
-import React from 'react'
+"use client";
+import { useEffect, useState } from "react";
+import { ReloadIcon } from "@radix-ui/react-icons";
+import { useSearchParams } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
+import GlobalFilter from "./GlobalFilter";
 
 const GlobalResult = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const searchParams = useSearchParams();
+  const [loading, setLoading] = useState(false);
+  const [result, setResult] = useState([
+    { type: "question", id: 1, title: "Next.js question" },
+    { type: "tag", id: 1, title: "Nextjs" },
+    { type: "user", id: 1, title: "moreno" },
+  ]);
+  const global = searchParams.get("global");
+  const type = searchParams.get("type");
 
-export default GlobalResult
+  useEffect(() => {
+    const fetchResult = async () => {
+      setResult([]);
+      setLoading(true);
+      try {
+      } catch (error) {
+        console.log(error);
+        throw error;
+      } finally {
+        setLoading(false);
+      }
+    };
+  }, [global, type]);
+
+  const renderLink = (type: string, id: string) => {
+    return "/";
+  };
+
+  return (
+    <div className="absolute top-full z-10 mt-3 w-full rounded-xl bg-light-800 py-5 shadow-sm dark:bg-dark-400">
+      <GlobalFilter />
+
+      <div className="my-5 h-px bg-light-700/50 dark:bg-dark-500/50"></div>
+      <div className="space-y-5">
+        <p className="text-dark400_light900 paragraph-semibold px-5">
+          Top match
+        </p>
+        {loading ? (
+          <div className="flex-center flex-col px-5">
+            <ReloadIcon className="my-2 size-10 animate-spin text-primary-500" />
+            <p className="text-dark200_light800 body-regular">
+              Browsing the entire database
+            </p>
+          </div>
+        ) : (
+          <div className="flex flex-col gap-2">
+            {result.length > 0 ? (
+              result.map((item: any, index: number) => (
+                <Link
+                  key={item.type + item.id + index}
+                  href={renderLink("type", "id")}
+                  className="flex w-full cursor-pointer items-start gap-3 px-5 py-2.5 hover:bg-light-700/50 dark:bg-dark-500/50"
+                >
+                  <Image
+                    src="/assets/icons/tag.svg"
+                    alt="tags"
+                    width={18}
+                    height={18}
+                    className="invert-colors mt-1 object-contain"
+                  />
+                  <div className="flex flex-col">
+                    <p className="body-medium text-dark200_light800 line-clamp-1">
+                      {item.title}
+                    </p>
+                    <p className="text-dark400_light800 small-medium mt-1 font-bold capitalize">
+                      {item.type}
+                    </p>
+                  </div>
+                </Link>
+              ))
+            ) : (
+              <div className="flex-center flex-col px-5">
+                <p className="text-dark200_light800 body-regular px-5">
+                  Oops! no result found
+                </p>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default GlobalResult;
